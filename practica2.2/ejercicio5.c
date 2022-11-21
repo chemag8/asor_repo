1c00ed39bef85dd8cd3b13d615f7cdad6dcc476f
chemag8 <73070596+chemag8@users.noreply.github.com>
2022-11-21 09:31:05 +0100

    Create ejercicio5.c

---
+++ practica2.2/ejercicio5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+
+  int df = open("/home/cursoredes/Documents/prueba/test.txt", O_CREAT, 0645);
+
+  return 1;
+}
+
+[cursoredes@localhost prueba]$ ls -ld test.txt 
+-rw-r--r-x 1 cursoredes cursoredes 0 Nov 21 09:29 test.txt
