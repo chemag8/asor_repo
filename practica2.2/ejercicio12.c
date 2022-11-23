462c08a1d1ed7f99c2ed12d1f1372f205936c6b8
chemag8 <73070596+chemag8@users.noreply.github.com>
2022-11-23 20:41:23 +0100

    Create ejercicio12.c

---
+++ practica2.2/ejercicio12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+	if (argc < 2) {
+		printf("ERROR: Falta la ruta del archivo.\n");
+		return -1;
+	}
+
+
+  int fd = open(argv[1], O_CREAT | O_RDWR, 0777);
+  if (fd == -1) {
+    printf("ERROR: No se ha podido abrir/crear el fichero.\n");
+    return -1;
+  }
+
+  int fd2 = dup2(fd, 1);
+
+  printf("La salida ha sido redirigida\n", argv[1]);
+
+  dup2(fd2, fd);
+
+  return 1;
+}
