084cfe1b1b0b9e6a3ac754645673c0ec09aa1292
chemag8 <73070596+chemag8@users.noreply.github.com>
2022-12-05 09:50:44 +0100

    Create ejercicio1.c

---
+++ practica2.4/ejercicio1.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+
+#define PIPE_W 1
+#define PIPE_R 0
+
+int main(int argc, char** argv){
+
+  if (argc < 2) {
+    printf("Error: Introduce los comandos\n");
+  }
+
+  int fd[2];
+
+  int pip = pipe(fd);
+
+  int p = fork();
+
+  switch (p) {
+    case -1:
+      perror("ERROR al hacerl el fork");
+      return 1;
+      break;
+    case 0:
+    //Hijo
+      dup2(fd[PIPE_R],0); //Cambiamos el descriptor a la entrada.
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      char *newargv[] = {argv[3],argv[4],NULL};
+      execvp(newargv[0],newargv);
+    break;
+    default:
+    //Padre
+      dup2(fd[PIPE_W],1); //Cambiamos el descriptor a la entrada.
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      char *newargv2[] = {argv[1],argv[2],NULL};
+      execvp(newargv2[0],newargv2);
+    break;
+  }
+  return 0;
+}
